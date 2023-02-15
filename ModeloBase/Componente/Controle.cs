8a074657f0764e0a926239500ea3b38acecc0f3d
Manoel Victor Silva Lira <manoelvictorzxc17@hotmail.com>
2023-02-15 20:44:15 -0300

    Implementado algorítmo de suavizasão de curvas

--- ModeloBase/Componente/Controle.cs
+++ ModeloBase/Componente/Controle.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Drawing;
+using System.Linq;
 using System.Windows.Forms;
 
 namespace ModeloBase.Componente
@@ -9,6 +10,7 @@ namespace ModeloBase.Componente
     {
         public Configuracoes Parametros = new Configuracoes();
         public Bobina[] Bobinas = new Bobina[0x2];
+        public SmoothBazier AlgoritmoBazier = new SmoothBazier();
 
         private readonly Argument[] Pontos = new Argument[0x2];
         private readonly List<Linha> Linhas = new List<Linha>();
@@ -91,17 +93,17 @@ namespace ModeloBase.Componente
 
         public partial class Configuracoes
         {
-            public double ESPACAMENTO_LIVRE         = 7d;
-            public double FATOR_CORRECAO_BAIXO      = 10d;
-            public double FATOR_CORRECAO_ALTO       = 0d;
+            public double ESPACAMENTO_LIVRE = 7d;
+            public double FATOR_CORRECAO_BAIXO = 10d;
+            public double FATOR_CORRECAO_ALTO = 0d;
             public double FATOR_CORRECAO_RAIO_MAIOR = 0.10d;
             public double FATOR_CORRECAO_RAIO_MENOR = 0.08d;
 
 
-            public int FATOR_CORRECAO_DECREMENTO    = 4;
-            public int FATOR_CORRECAO_LIMITACAO     = 8;
-            public int POINT_SIZE                   = 10;
-            public int RENDER_POINTS                = 8;
+            public int FATOR_CORRECAO_DECREMENTO = 4;
+            public int FATOR_CORRECAO_LIMITACAO = 8;
+            public int POINT_SIZE = 10;
+            public int RENDER_POINTS = 32;
         }
 
         public partial class Bobina
@@ -135,9 +137,11 @@ namespace ModeloBase.Componente
                 RazaoVariavel += Razao;
             }
 
+            Point[] Pnts = new Point[PontosList.Count];
             for (int i = 0; i < PontosList.Count; i++)
             {
-
+                Pnts[i] = ConvertPoint(PontosList[i]);
+                /*
                 if (PontosList.Count == (i + 1))
                 {
                     G.DrawLine(Color, ConvertPoint(PontosList[i - 1]), ConvertPoint(PontosList[i]));
@@ -146,8 +150,10 @@ namespace ModeloBase.Componente
                 else
 
                     G.DrawLine(Color, ConvertPoint(PontosList[i]), ConvertPoint(PontosList[i + 1]));
+                */
             }
 
+            G.DrawCurve(Color, Pnts);
             PontosList.Clear();
         }
         private void DoDraw(ref Graphics G, int Raio, int Pontos, double Begin, double End, Pen Color = null)
@@ -165,14 +171,22 @@ namespace ModeloBase.Componente
                 RazaoVariavel += Razao;
             }
 
+            PointF[] Pnts = new PointF[PontosList.Count];
             for (int i = 0; i < PontosList.Count; i++)
             {
+                Pnts[i] = ConvertPointF(PontosList[i]);
+                /*
                 if (PontosList.Count == (i + 1))
                     G.DrawLine(Pen, ConvertPoint(PontosList[i - 1]), ConvertPoint(PontosList[i]));
                 else
                     G.DrawLine(Pen, ConvertPoint(PontosList[i]), ConvertPoint(PontosList[i + 1]));
+                */
             }
 
+            var LIST = Pnts.ToList();
+            var TEMP = AlgoritmoBazier.SmoothCurve(LIST).ToArray();
+            G.DrawCurve(Pen, TEMP);
+            //G.DrawCurve(Pen, Pnts);
             PontosList.Clear();
         }
         private void DrawWithSpace(int Number, ref Graphics G, int raio, int pontos, bool Pri = true, Pen Color = null)
@@ -268,6 +282,11 @@ namespace ModeloBase.Componente
         {
             return new Point((int)P.PointX, (int)P.PointY);
         }
+        public PointF ConvertPointF(Ponto P)
+        {
+            return new PointF((float)P.PointX, (float)P.PointY);
+        }
+
         private Quadrante GetLocation(double Angle)
         {
             if (Angle >= 0 && Angle < ConvertToRadius(90))
@@ -374,6 +393,59 @@ namespace ModeloBase.Componente
             II = 2,
             III = 3,
             IV = 4
+        }
+    }
+
+    public partial class SmoothBazier
+    {
+        public List<PointF> SmoothCurve(List<PointF> points)
+        {
+            List<PointF> smoothedPoints = new List<PointF>();
+
+            if (points.Count < 2)
+            {
+                smoothedPoints.AddRange(points);
+            }
+            else
+            {
+                smoothedPoints.Add(points[0]);
+
+                for (int i = 0; i < points.Count - 1; i++)
+                {
+                    PointF pt1 = points[i];
+                    PointF pt2 = points[i + 1];
+
+                    float distance = (float)Math.Sqrt(Math.Pow(pt2.X - pt1.X, 2) + Math.Pow(pt2.Y - pt1.Y, 2));
+                    PointF midPoint = new PointF((pt1.X + pt2.X) / 2, (pt1.Y + pt2.Y) / 2);
+                    float step = distance / 4;
+
+                    PointF diff1 = new PointF(midPoint.X - pt1.X, midPoint.Y - pt1.Y);
+                    PointF tangent1 = new PointF(diff1.X / step, diff1.Y / step);
+
+                    PointF diff2 = new PointF(pt2.X - midPoint.X, pt2.Y - midPoint.Y);
+                    PointF tangent2 = new PointF(diff2.X / step, diff2.Y / step);
+
+                    smoothedPoints.Add(pt1);
+
+                    for (int j = 1; j <= 4; j++)
+                    {
+                        float t = (float)j / 4;
+                        float t2 = t * t;
+                        float t3 = t2 * t;
+                        float c1 = 2 * t3 - 3 * t2 + 1;
+                        float c2 = -2 * t3 + 3 * t2;
+                        float c3 = t3 - 2 * t2 + t;
+                        float c4 = t3 - t2;
+                        float x = c1 * pt1.X + c2 * pt2.X + c3 * tangent1.X + c4 * tangent2.X;
+                        float y = c1 * pt1.Y + c2 * pt2.Y + c3 * tangent1.Y + c4 * tangent2.Y;
+                        smoothedPoints.Add(new PointF(x, y));
+                    }
+                }
+
+                smoothedPoints.Add(points[points.Count - 1]);
+            }
+
+            return smoothedPoints;
         }
     }
 }
